8779a8f422af5a97d67ac3c73d4dffe96f8b07e0
ijkmoises <ijkmoises@gmail.com>
2020-12-11 12:11:33 -0300

    Initial

---
+++ app/src/main/java/com/c6bank/funtamentals/android/poc_ci/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.c6bank.funtamentals.android.poc_ci
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
