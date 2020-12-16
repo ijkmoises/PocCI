bd11c04c846bb5af90ac993f6bb248f84088c4ca
ijkmoises <ijkmoises@gmail.com>
2020-12-16 18:54:32 -0300

    add script ifconfig

--- app/execute_local_tests.sh
+++ app/execute_local_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 apt install -y net-tools
 #ifconfig eth0 192.168.0.1 netmask 255.255.255.0
-ifconfig wka1ddag70ie-1 192.168.15.28 netmask 255.255.255.0
+echo wka1ddag70ie
+ifconfig
+#ifconfig wka1ddag70ie-1 192.168.15.28 netmask 255.255.255.0
 ping 192.168.15.5
 
