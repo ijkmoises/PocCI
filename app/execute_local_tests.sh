06de7caa4345c4dbea75dbb9bccd970565e9b891
ijkmoises <ijkmoises@gmail.com>
2020-12-17 15:47:06 -0300

    add script ifconfig

--- app/execute_local_tests.sh
+++ app/execute_local_tests.sh
@@ -2,7 +2,7 @@
 apt-get install -y net-tools
 apt-get install -y iputils-ping
 apt-get install -y sshpass
-apt install iptables-persistent netfilter-persistent
+apt-get install -y iptables-persistent netfilter-persistent
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no moises@192.168.15.5
 #sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no moises@192.168.15.5
 iptables -t nat -i -I PREROUTING -p tcp -d 192.168.15.5 - dport 5585 -j DNAT - to-destination 127.0.0.1:5555
