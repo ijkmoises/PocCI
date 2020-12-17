12cdfbeb455c0fc812ede908a2f84aeabb84c991
ijkmoises <ijkmoises@gmail.com>
2020-12-17 15:41:14 -0300

    add script ifconfig

--- app/execute_local_tests.sh
+++ app/execute_local_tests.sh
@@ -3,7 +3,9 @@ apt-get install -y net-tools
 apt-get install -y iputils-ping
 apt-get install -y sshpass
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no moises@192.168.15.5
-sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no moises@192.168.15.5
-
+#sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no moises@192.168.15.5
+sudo iptables -t nat -i -I PREROUTING -p tcp -d 192.168.15.5 - dport 5585 -j DNAT - to-destination 127.0.0.1:5555
+adb connect 192.168.15.5:5585
+adb devices
 
 
