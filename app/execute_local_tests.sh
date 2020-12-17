fa2e5ccc4e23ec3aebe60efcd46ffff791ae076d
ijkmoises <ijkmoises@gmail.com>
2020-12-17 16:04:02 -0300

    add script ifconfig

--- app/execute_local_tests.sh
+++ app/execute_local_tests.sh
@@ -3,12 +3,13 @@
 #apt-get install -y iputils-ping
 #apt-get install -y sshpass
 sudo apt install -y sudo
-apt-get install -y iptables
+
 clear
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no moises@192.168.15.5
 #sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no moises@192.168.15.5
 echo do iptables to redirect all incoming connections on port 5585 to localhost:5555
-iptables -t nat -i -I PREROUTING -p tcp -d 192.168.15.5 --dport 5585 -j DNAT - to-destination 127.0.0.1:5555
+iptables -t nat -i -I PREROUTING -p tcp -d 192.168.15.5 --dport 5585 -j DNAT --to-destination 127.0.0.1:5555
+
 clear
 
 echo try connect remote avd...
