6bd648ae425267b05504517dc90faebfff153f99
ijkmoises <ijkmoises@gmail.com>
2020-12-17 13:52:14 -0300

    add script

--- app/execute_local_tests.sh
+++ app/execute_local_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 apt-get install -y net-tools
 apt-get install -y iputils-ping
-#ifconfig eth0 192.168.0.1 netmask 255.255.255.0
-#ifconfig wka1ddag70ie-1 192.168.15.28 netmask 255.255.255.0
-ping 192.168.15.22
+apt-get install -y sshpass
+#ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no moises@192.168.15.5
+sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no moises@192.168.15.5
+
+
 
