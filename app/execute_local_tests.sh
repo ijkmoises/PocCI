cda67ab2140882dbfc56f798001c9c886b807057
ijkmoises <ijkmoises@gmail.com>
2020-12-17 17:44:28 -0300

    add script ifconfig

--- app/execute_local_tests.sh
+++ app/execute_local_tests.sh
@@ -3,13 +3,13 @@
 #apt-get install -y iputils-ping
 #apt-get install -y sshpass
 
-apt-get install -y iptables
+#apt-get install -y iptables
 clear
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no moises@192.168.15.5
 #sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no moises@192.168.15.5
 
 #iptables -t nat -i -I PREROUTING -p tcp -d 192.168.15.5 --dport 5585 -j DNAT --to-destination 127.0.0.1:5555
-iptables -t nat -A PREROUTING -p tcp -d 192.168.15.5 --deport 5585 -j REDIRECT --to-ports 5555
+
 clear
 
 
