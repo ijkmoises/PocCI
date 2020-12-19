731dccfe33660df412ba65a272178c42758d83b4
ijkmoises <ijkmoises@gmail.com>
2020-12-19 15:21:16 -0300

    add script ifconfig

--- app/execute_local_tests.sh
+++ app/execute_local_tests.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-whoami
+#whoami
 #apt-get install -y net-tools
 #apt-get install -y iputils-ping
-apt-get install -y sshpass
+#apt-get install -y sshpass
 
 #apt-get install -y iptables
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no moises@192.168.15.5
 #sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no moises@192.168.15.5
 
-sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no -NL 5554:localhost:5554 -L 5555:localhost:5555 moises@192.168.15.5 -t
+#sshpass -p "Barato10#" ssh -o StrictHostKeyChecking=no -NL 5554:localhost:5554 -L 5555:localhost:5555 moises@192.168.15.5 -t
 
 #adb tcpip 5555
-hostname -I
+#hostname -I
+
+adb connect 192.168.15.5:5555
